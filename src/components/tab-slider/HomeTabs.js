3132f763f48727e40d4a19dacb9c5c59518ed0e4
muzammalrahim <creative.joomdev@gmail.com>
2022-03-17 11:15:16 +0500

    home-page

--- src/components/tab-slider/HomeTabs.js
+++ src/components/tab-slider/HomeTabs.js
@@ -9,7 +9,6 @@ import PL1 from '../../images/product_1.png';
 import PL2 from '../../images/product_2.png';
 import PL3 from '../../images/product_3.png';
 import ViewModal from '../view-product-modal/ViewModal'
-import { Link } from 'react-router-dom';
 
 
 
@@ -57,71 +56,67 @@ export default function HomeTabs() {
         <Tab eventKey="Headphone" title="Headphone">
             <Slider {...settings}>
                 <div>
-                    <Link to="/product-detail">
-                        <div className="product_box_info">
-                            <span className="stock-tag">New</span>
-                            <div className="overlay_icons">
-                                <span className="heart-icon"><FaHeart /><ViewModal/></span>
-                                <span className="exchange-icon"><FaExchangeAlt /></span>
-                                <span className="eye-icon"><FaEye /></span>
-                            </div>
-                            <div className="Product_img">
-                                <img src={PL2} alt="product" />
-                            </div>
-                            <div className="Product_text clearfix">
-                                <ul className="rating_star">
-                                    <li className="deal_hot">
-                                        <i className="fa fa-star yellow"></i>
-                                        <i className="fa fa-star yellow"></i>
-                                        <i className="fa fa-star gray"></i>
-                                        <i className="fa fa-star gray"></i>
-                                        <i className="fa fa-star gray"></i>
-                                        <span>(2 Review) </span>
-                                    </li>
-                                </ul>
-                                <h2>New Apple iphone 12 pro Max Gold</h2>
-                                <div className="price-tag">
-                                    <span className="old_price">$123.21</span>
-                                    <span className="new_price">$123.21</span>
-                                </div>
-                                <button type="button" className="add-trolley">Add to Trolley</button>
-                            </div>
+                    <div className="product_box_info">
+                        <span className="stock-tag">New</span>
+                        <div className="overlay_icons">
+                            <span className="heart-icon"><FaHeart /></span>
+                            <span className="exchange-icon"><FaExchangeAlt /></span>
+                            <span className="eye-icon"><FaEye /><ViewModal/></span>
                         </div>
-                    </Link> 
+                        <div className="Product_img">
+                            <img src={PL2} alt="product" />
+                        </div>
+                        <div className="Product_text clearfix">
+                            <ul className="rating_star">
+                                <li className="deal_hot">
+                                    <i className="fa fa-star yellow"></i>
+                                    <i className="fa fa-star yellow"></i>
+                                    <i className="fa fa-star gray"></i>
+                                    <i className="fa fa-star gray"></i>
+                                    <i className="fa fa-star gray"></i>
+                                    <span>(2 Review) </span>
+                                </li>
+                            </ul>
+                            <h2>New Apple iphone 12 pro Max Gold</h2>
+                            <div className="price-tag">
+                                <span className="old_price">$123.21</span>
+                                <span className="new_price">$123.21</span>
+                            </div>
+                            <button type="button" className="add-trolley">Add to Trolley</button>
+                        </div>
+                    </div>
                 </div>
+
                 <div>
-                    <Link to="/product-detail">
-                        <div className="product_box_info">
-                            <span className="stock-tag">New</span>
-                            <div className="overlay_icons">
-                                <span className="heart-icon"><FaHeart /></span>
-                                <span className="exchange-icon"><FaExchangeAlt /></span>
-                                <span className="eye-icon"><FaEye /></span>
-                            </div>
-                            <div className="Product_img">
-                                <img src={PL3} alt="product" />
-                            </div>
-                            <div className="Product_text clearfix">
-                                <ul className="rating_star">
-                                    <li className="deal_hot">
-                                        <i className="fa fa-star yellow"></i>
-                                        <i className="fa fa-star yellow"></i>
-                                        <i className="fa fa-star gray"></i>
-                                        <i className="fa fa-star gray"></i>
-                                        <i className="fa fa-star gray"></i>
-                                        <span>(2 Review) </span>
-                                    </li>
-                                </ul>
-                                <h2>New Apple iphone 12 pro Max Gold</h2>
-                                <div className="price-tag">
-                                    <span className="old_price">$123.21</span>
-                                    <span className="new_price">$123.21</span>
-                                </div>
-                                <button type="button" className="add-trolley">Add to Trolley</button>
-                            </div>
+                    <div className="product_box_info">
+                        <span className="stock-tag">New</span>
+                        <div className="overlay_icons">
+                            <span className="heart-icon"><FaHeart /></span>
+                            <span className="exchange-icon"><FaExchangeAlt /></span>
+                            <span className="eye-icon"><FaEye /></span>
                         </div>
-                    </Link>
-                    
+                        <div className="Product_img">
+                            <img src={PL3} alt="product" />
+                        </div>
+                        <div className="Product_text clearfix">
+                            <ul className="rating_star">
+                                <li className="deal_hot">
+                                    <i className="fa fa-star yellow"></i>
+                                    <i className="fa fa-star yellow"></i>
+                                    <i className="fa fa-star gray"></i>
+                                    <i className="fa fa-star gray"></i>
+                                    <i className="fa fa-star gray"></i>
+                                    <span>(2 Review) </span>
+                                </li>
+                            </ul>
+                            <h2>New Apple iphone 12 pro Max Gold</h2>
+                            <div className="price-tag">
+                                <span className="old_price">$123.21</span>
+                                <span className="new_price">$123.21</span>
+                            </div>
+                            <button type="button" className="add-trolley">Add to Trolley</button>
+                        </div>
+                    </div>
                 </div>
 
                 <div>
